9ea46b1e95d3bcf8b9218ecd1d246dbe3dbf7101
Bruno Cabral <cabral.brc@gmail.com>
2026-05-05 16:57:11 -0700

    `Player` movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,15 +2,23 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public Vector3 movementDirection;
+
+    private CharacterController characterController;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
-        
+        characterController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        movementDirection.x = Input.GetAxisRaw("Horizontal");
+        movementDirection.z = Input.GetAxisRaw("Vertical");
+        movementDirection = movementDirection.normalized;
+
+        characterController.Move(movementDirection * Time.deltaTime);
     }
 }
