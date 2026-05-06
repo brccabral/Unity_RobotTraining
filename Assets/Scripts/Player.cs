d28421c1adefd1214df2b31a022c07acc10b0444
Bruno Cabral <cabral.brc@gmail.com>
2026-05-05 17:46:06 -0700

    Add player movement and camera look controls

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,13 +3,18 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public Vector3 movementDirection;
+    [SerializeField] private float moveSpeed = 5f;
+    public Vector3 lookRotationDirection;
+    [SerializeField] private float lookSensitivity = 100f;
 
     private CharacterController characterController;
+    private Camera firstPersonCamera;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
         characterController = GetComponent<CharacterController>();
+        firstPersonCamera = GetComponentInChildren<Camera>();
     }
 
     // Update is called once per frame
@@ -19,6 +24,15 @@ public class Player : MonoBehaviour
         movementDirection.z = Input.GetAxisRaw("Vertical");
         movementDirection = movementDirection.normalized;
 
-        characterController.Move(movementDirection * Time.deltaTime);
+        var forward = characterController.transform.forward * movementDirection.z;
+        var right = characterController.transform.right * movementDirection.x;
+        characterController.Move((forward + right) * (Time.deltaTime * moveSpeed));
+
+        lookRotationDirection.y += Input.GetAxisRaw("Mouse X") * (Time.deltaTime * lookSensitivity);
+        lookRotationDirection.x -= Input.GetAxisRaw("Mouse Y") * (Time.deltaTime * lookSensitivity);
+        lookRotationDirection.x = Mathf.Clamp(lookRotationDirection.x, -80, 80);
+
+        firstPersonCamera.transform.localEulerAngles = new Vector3(lookRotationDirection.x, 0, 0);
+        characterController.transform.eulerAngles = new Vector3(0, lookRotationDirection.y, 0);
     }
 }
