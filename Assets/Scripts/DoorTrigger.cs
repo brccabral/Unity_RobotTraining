ab9d11731fd8e3eaec0207a1be7d96f0896ac27d
Bruno Cabral <cabral.brc@gmail.com>
2026-05-12 16:29:21 -0700

    Add `OnTriggerExit` to reactivate door when leaving trigger area

--- Assets/Scripts/DoorTrigger.cs
+++ Assets/Scripts/DoorTrigger.cs
@@ -9,4 +9,9 @@ public class DoorTrigger : MonoBehaviour
     {
         door.SetActive(false);
     }
+
+    private void OnTriggerExit(Collider other)
+    {
+        door.SetActive(true);
+    }
 }
