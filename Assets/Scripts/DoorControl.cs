94f8996e5fc5590d3acca35710556532adc5483b
Bruno Cabral <cabral.brc@gmail.com>
2026-05-12 18:05:49 -0700

    Add `DoorControl` script to manage door locking/unlocking

---
+++ Assets/Scripts/DoorControl.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class DoorControl : MonoBehaviour
+{
+    [SerializeField] private DoorTrigger doorTrigger;
+    [SerializeField] private MeshRenderer doorMesh;
+
+    [SerializeField] private bool unlocked;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (unlocked)
+        {
+            Unlock();
+        }
+        else
+        {
+            Lock();
+        }
+    }
+
+    private void Unlock()
+    {
+        unlocked = true;
+        doorMesh.material.color = Color.blue;
+        doorTrigger.gameObject.SetActive(true);
+    }
+
+    private void Lock()
+    {
+        unlocked = false;
+        doorMesh.material.color = Color.red;
+        doorTrigger.gameObject.SetActive(false);
+    }
+}
