34fa71c1e5a2a43138f5726b23ff37b306ed1d74
Bruno Cabral <cabral.brc@gmail.com>
2026-05-12 17:57:00 -0700

    Integrate door opening/closing animation using Animator, update `DoorTrigger` logic for animation control

--- Assets/Scripts/DoorTrigger.cs
+++ Assets/Scripts/DoorTrigger.cs
@@ -3,15 +3,16 @@ using UnityEngine;
 
 public class DoorTrigger : MonoBehaviour
 {
-    [SerializeField] private GameObject door;
+    private static readonly int IsOpen = Animator.StringToHash("IsOpen");
+    [SerializeField] private Animator doorAnimator;
 
     private void OnTriggerEnter(Collider other)
     {
-        door.SetActive(false);
+        doorAnimator.SetBool(IsOpen, true);
     }
 
     private void OnTriggerExit(Collider other)
     {
-        door.SetActive(true);
+        doorAnimator.SetBool(IsOpen, false);
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -16,8 +16,8 @@ public class Player : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Awake()
     {
-        Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked;
+        // Cursor.visible = false;
+        // Cursor.lockState = CursorLockMode.Locked;
 
         firstPersonCamera = GetComponentInChildren<Camera>();
         characterController = GetComponent<CharacterController>();
