51050f56a35cfcb3e44a125083e25f447a0a3927
Bruno Cabral <cabral.brc@gmail.com>
2026-05-14 16:49:47 -0700

    Add `PlayerInteractorModule` for raycast-based interaction system, integrate into Camera

---
+++ Assets/Scripts/PlayerInteractorModule.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class PlayerInteractorModule : MonoBehaviour
+{
+    [SerializeField] private Transform interationRayOrigin;
+    [SerializeField] private float interactionRange;
+    [SerializeField] private LayerMask interactableLayers;
+
+    private void Update()
+    {
+        Ray ray = new Ray(interationRayOrigin.position, interationRayOrigin.forward);
+        RaycastHit hitInfo;
+        if (Physics.Raycast(ray, out hitInfo, interactionRange, interactableLayers))
+        {
+            Debug.Log($"Hit {hitInfo.collider.name}");
+        }
+    }
+}
