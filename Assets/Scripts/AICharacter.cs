a168d2e080de886a20d4fbf745bfc709e0a5ad5c
Bruno Cabral <cabral.brc@gmail.com>
2026-05-20 16:50:13 -0700

    AICharacter follows player

--- Assets/Scripts/AICharacter.cs
+++ Assets/Scripts/AICharacter.cs
@@ -9,6 +9,19 @@ public class AICharacter : MonoBehaviour
 
     private void Start()
     {
+        InvokeRepeating(nameof(UpdateDestination), 0.2f, 1f);
+    }
+
+    private void UpdateDestination()
+    {
         agent.SetDestination(goal.position);
+    }
+
+    private void Update()
+    {
+        if (Vector3.Distance(transform.position, goal.position) < 1)
+        {
+            // TODO: interact with the player 
+        }
     }
 }
