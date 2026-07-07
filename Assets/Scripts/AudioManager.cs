51259fcb6e5f041d9f48864451f5240af28f8578
Bruno Cabral <cabral.brc@gmail.com>
2026-07-06 17:35:22 -0700

    add projectile spawn sound effect and integrate with ShootingModule

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -5,6 +5,7 @@ public class AudioManager : MonoBehaviour
     [SerializeField] private AudioSource backgroundMusic;
     [SerializeField] private AudioSource doorLock;
     [SerializeField] private AudioSource doorOpenClose;
+    [SerializeField] private AudioSource projectileSpawn;
 
     public void PlayDoorLock(AudioClip clip)
     {
@@ -14,5 +15,10 @@ public class AudioManager : MonoBehaviour
     public void PlayDoorOpenClose(AudioClip clip)
     {
         doorOpenClose.PlayOneShot(clip);
+    }
+    
+    public void PlayProjectileSpawn(AudioClip clip)
+    {
+        projectileSpawn.PlayOneShot(clip);
     }
 }
--- Assets/Scripts/ShootingModule.cs
+++ Assets/Scripts/ShootingModule.cs
@@ -5,6 +5,17 @@ public class ShootingModule : MonoBehaviour
     [SerializeField] private ProjectilePooling projectilePool;
     [SerializeField] private Transform weaponTip;
 
+    [SerializeField] private AudioClip projectileSpawnSound;
+    private AudioManager audioManager;
+
+    private void Start()
+    {
+        if (!audioManager)
+        {
+            audioManager = FindAnyObjectByType<AudioManager>();
+        }
+    }
+
     public void Shoot()
     {
         var projectile = projectilePool.RetrieveProjectile();
@@ -12,5 +23,7 @@ public class ShootingModule : MonoBehaviour
         projectile.transform.rotation = weaponTip.rotation;
         projectile.gameObject.SetActive(true);
         projectile.StartBullet();
+
+        audioManager.PlayProjectileSpawn(projectileSpawnSound);
     }
 }
